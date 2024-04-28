0fea008ee927ad1fe2f6a708dffc0f323854efa7
shashankgopalakrishna <shashankgbydher@gmail.com>
2024-04-27 18:57:04 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const cors = require("cors");
+const userRoutes = require("./routes/user.js")
+const schedule = require("./routes/schedule.js")
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(helmet())
+
+app.use("/api", userRoutes)
+app.use("/api", schedule)
+
+// -------
+const mongoose = require('mongoose');
+
+const itemsRouter = require('./routes/schedule.js');
+
+// Connect to MongoDB
+// mongoose.connect('mongodb://127.0.0.1:27017/officehourscheduler', {
+mongoose.connect('mongodb+srv://alishagaikwad03:016hRZDGX4xCBWOE@cluster0.zmq63nq.mongodb.net/officehourscheduler', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+  process.exit(1);
+});
+
+// Routes
+app.use('/api/items', itemsRouter);
+
+app.use(express.static("./CN-Project-Frontend/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "CN-Project-Frontend", "build", "index.html"))
+})
+
+// -------
+
+app.listen(PORT, () => console.log("Listening"));
+
+
