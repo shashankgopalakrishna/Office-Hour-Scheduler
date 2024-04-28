c37df0e15731716424db256d521ff1808faaf906
shashankgopalakrishna <shashankgbydher@gmail.com>
2024-04-27 19:16:34 -0700

    env file changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const userRoutes = require("./routes/user.js")
 const schedule = require("./routes/schedule.js")
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
