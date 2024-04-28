0b429be3c4c97c2ebf0924d231d113c2f8b0b6d3
shashankgopalakrishna <shashankgbydher@gmail.com>
2024-04-27 19:14:09 -0700

    Deleted nodemodules

--- node_modules/is-iso-date/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = isISODate;
-
-var isoDateRegExp = new RegExp( /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/ );
-
-function isISODate( str ) {
-    return isoDateRegExp.test( str );
-}
